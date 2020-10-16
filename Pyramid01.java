8b59ed1b5258cc58d56eedcc2329ae56dcba6822
NikolaiMihov25 <72207939+NikolaiMihov25@users.noreply.github.com>
2020-10-16 18:34:41 +0300

    Add files via upload

---
+++ Pyramid01.java
@@ -0,0 +1,23 @@
+package edu.smg;
+
+import java.util.Scanner;
+
+public class Pyramid01 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for (int i = n; i>0; i--) {
+			for(int j = i; j>=1; j--) {
+				System.out.print(j);
+			}
+			for(int j = 2; j<=i; j++) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+	}
+
+	}
+
+
